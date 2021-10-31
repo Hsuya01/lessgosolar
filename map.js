effa8401c976540a5e21122d238a7d67456b9f55
Shwaas Adlakha <shwaasadlakha@Shwaass-MacBook-Pro.local>
2021-10-31 05:44:18 +0530

    Files added and modified

--- map.js
+++ map.js
@@ -17,11 +17,32 @@ var map = L.map("map", {
     maxBounds: [[-120, -220], [120, 220]],
     layers: [OpenStreetMap_Mapnik]})}
 
+var popup = L.popup()
+    .setLatLng(latlng)
+    .setContent('<p>Hello world!<br />This is a nice popup.</p>')
+    .openOn(map);
+
+marker.bindPopup(popupContent).openPopup();
+
+var elem = document.querySelector('input[type="range"]');
+
+var rangeValue = function(){
+  var newValue = "Time:" + elem.value + " Years";
+  var target = document.querySelector('.value');
+  target.innerHTML = newValue;
+}
+
+document.getElementById("range").onInput(elem.addEventListener("input", rangeValue));
+
+
+console.log(document.getElementById("electric").value)
+
 const latitude = 30;
 const longitude = 31;
 
-"https://developer.nrel.gov/api/solar/solar_resource/v1.xml?api_key=S0OgpQV2Coclte3StxZXW0zXQHefPMdwjxbTJ5l1&lat=${latitude}&lon=${longitude}"
+// "https://developer.nrel.gov/api/solar/solar_resource/v1.xml?api_key=S0OgpQV2Coclte3StxZXW0zXQHefPMdwjxbTJ5l1&lat=${latitude}&lon=${longitude}"
 
-const data = fetch();
+// const data = fetch();
 
-console.log(data)
+
+// console.log(data)
